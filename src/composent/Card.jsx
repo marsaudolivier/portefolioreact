22ba5f5745b1b14c8a359612b531cbcdb8095da4
Marsaud olivier <cougies79700@gmail.com>
2024-03-19 13:57:41 +0100

    add btn to card

--- src/composent/Card.jsx
+++ src/composent/Card.jsx
@@ -1,6 +1,40 @@
 import React from "react";
 
 const Card = () => {
+  // project Vparro 1
+  const redirectToProject1 = () => {
+    window.location.href = "https://marsaudolivier.alwaysdata.net/par";
+  };
+  const redirectToGithub1 = () => {
+    window.location.href = "https://github.com/marsaudolivier/ECF_MarsaudOlivier";
+  };
+ // project Vparro 2
+  const redirectToProject2 = () => {
+    window.location.href =
+      "https://ecfparrotv2-3bphjzfh5-marsaudoliviers-projects.vercel.app/";
+  };
+  const redirectToGithub2 = () => {
+    window.open("https://github.com/marsaudolivier/ParrotV2_Front");
+  };
+  const redirectToGithub21 = () => {
+    window.open("https://github.com/marsaudolivier/ParrotV2_Back_Node");
+  }
+  // project Association
+  const redirectToProject3 = () => {
+    window.location.href = "http://marsaudolivier.me/Juste-un-coup-de-patte/";
+  };
+  const redirectToGithub3 = () => {
+    window.location.href = "https://github.com/marsaudolivier/Juste-un-coup-de-patte";
+  };
+  // project jeux de dé
+  const redirectToProject4 = () => {
+    window.location.href = "http://marsaudolivier.me/Evaluation-JS-Jeux-de-d-/";
+  };
+  const redirectToGithub4 = () => {
+    window.location.href = "https://github.com/marsaudolivier/Evaluation-JS-Jeux-de-d-";
+  }
+
+
   return (
     <section className="projects">
       <div className="container">
@@ -12,16 +46,25 @@ const Card = () => {
               alt="Garage ParrotV1"
               className="project_img"
             />
-
             <h3 className="project_title">Garage Parrot V1</h3>
             <p className="project_description">
               Site de vente de voiture Ceci est mon sujet examen présenté pour
               mon graduate version PHP
             </p>
-            <a href="https://marsaudolivier.alwaysdata.net/par">
-              {" "}
+            <button
+              className="card_btn"
+              type="button"
+              onClick={redirectToProject1}
+            >
               voir le projet
-            </a>
+            </button>
+            <button
+              className="card_btn"
+              type="button"
+              onClick={redirectToGithub1}
+            >
+              voir le Github
+            </button>
           </div>
           <div className="project">
             <img
@@ -34,10 +77,27 @@ const Card = () => {
               Site de vente de voiture Ceci est mon sujet examen présenté pour
               mon graduate version REACT.js + Node.js
             </p>
-            <a href="https://ecfparrotv2-3bphjzfh5-marsaudoliviers-projects.vercel.app/">
-              {" "}
+            <button
+              className="card_btn"
+              type="button"
+              onClick={redirectToProject2}
+            >
               voir le projet
-            </a>
+            </button>
+            <button
+              className="card_btn"
+              type="button"
+              onClick={redirectToGithub2}
+            >
+              voir le Github Coté Front
+            </button>
+            <button
+              className="card_btn"
+              type="button"
+              onClick={redirectToGithub21}
+            >
+              voir le Github Coté back
+            </button>
           </div>
           <div className="project">
             <img
@@ -49,10 +109,20 @@ const Card = () => {
             <p className="project_description">
               Site association Premier site association pour mon graduate
             </p>
-            <a href="http://marsaudolivier.me/Juste-un-coup-de-patte/">
-              {" "}
+            <button
+              className="card_btn"
+              type="button"
+              onClick={redirectToProject3}
+            >
               voir le projet
-            </a>
+            </button>
+            <button
+              className="card_btn"
+              type="button"
+              onClick={redirectToGithub3}
+            >
+              voir le Github
+            </button>
           </div>
           <div className="project">
             <img
@@ -62,10 +132,20 @@ const Card = () => {
             />
             <h3 className="project_title">Jeux de dé</h3>
             <p className="project_description">Jeux de dé</p>
-            <a href="http://marsaudolivier.me/Evaluation-JS-Jeux-de-d-/">
-              {" "}
-              voir le projet
-            </a>
+            <button
+              className="card_btn"
+              type="button"
+              onClick={redirectToProject4}
+            >
+              voir les détails
+            </button>
+            <button
+              className="card_btn"
+              type="button"
+              onClick={redirectToGithub4}
+            >
+              voir le Github
+            </button>
           </div>
         </div>
       </div>
