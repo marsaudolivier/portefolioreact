b534498f02b199bfef0994592d3d99286592e462
Marsaud olivier <cougies79700@gmail.com>
2024-03-23 14:01:14 +0100

    try

--- src/composent/Service.jsx
+++ src/composent/Service.jsx
@@ -12,24 +12,22 @@ const Service = () => {
               <h2>1000€*</h2>
             </div>
             <h4>Développé Sous WORDPRESS</h4>
-            <ul >
-              <li >
-                Charte graphique à partir d'une maquette pré-définie.
-              </li>
+            <ul>
+              <li>Charte graphique à partir d'une maquette pré-définie.</li>
               <li>Sélection d'une palette de couleur</li>
-              <li >Domaine + hébergement pendant 12 mois</li>
+              <li>Domaine + hébergement pendant 12 mois</li>
               <li>Interface administrateur STANDARD</li>
-              <li >
+              <li>
                 Site Responsive <br /> (Adapté pour tous supports)
               </li>
               <li>
                 Mise en place du contenu <br /> (Support fournis par vos soin)
               </li>
-              <li >Formulaire de contact</li>
+              <li>Formulaire de contact</li>
               <li>Mise en place des lien réseaux sociaux</li>
-              <li >Optimisation du référencement (SEO)</li>
-              <li >Mise en ligne du site</li>
-              <li >Formation utilisation du site</li>
+              <li>Optimisation du référencement (SEO)</li>
+              <li>Mise en ligne du site</li>
+              <li>Formation utilisation du site</li>
             </ul>
           </div>
           <div className="Services">
@@ -38,71 +36,96 @@ const Service = () => {
               <h2>1800€*</h2>
             </div>
             <h4>Développé Sur mesure</h4>
-            <ul >
-              <li >
-                Charte graphique personnaliser
-              </li>
+            <ul>
+              <li>Charte graphique personnaliser</li>
               <li>Proposition de logo</li>
-              <li >Domaine + hébergement pendant 12 mois</li>
+              <li>Domaine + hébergement pendant 12 mois</li>
               <li>Interface administrateur sur mesure</li>
-              <li >
+              <li>
                 Site Responsive <br /> (Adapté pour tous supports)
               </li>
               <li>
                 Mise en place du contenu <br /> (Support fournis par vos soin)
               </li>
-              <li >Formulaire de contact & Map </li>
+              <li>Formulaire de contact & Map </li>
               <li>Mise en place des lien réseaux sociaux</li>
-              <li >Optimisation du référencement (SEO)</li>
-              <li >Mise en ligne du site</li>
-              <li >Formation utilisation du site  <br /> Personalisé</li>
+              <li>Optimisation du référencement (SEO)</li>
+              <li>Mise en ligne du site</li>
+              <li>
+                Formation utilisation du site <br /> Personalisé
+              </li>
             </ul>
           </div>
           <div className="Services">
             <div className="Service">
-            <h3>A partir de: </h3>
+              <h3>A partir de: </h3>
               <h2>3000€*</h2>
             </div>
-            <h4>Développé Sur mesure  <br /> Version Premium <br />solution E-commerce</h4>
-            <ul >
-              <li >
-                Charte graphique personnaliser
-              </li>
+            <h4>
+              Développé Sur mesure <br /> Version Premium <br />
+              solution E-commerce
+            </h4>
+            <ul>
+              <li>Charte graphique personnaliser</li>
               <li>Proposition de logo</li>
-              <li >Domaine + hébergement pendant 12 mois</li>
+              <li>Domaine + hébergement pendant 12 mois</li>
               <li>Interface administrateur sur mesure</li>
-              <li >
+              <li>
                 Site Responsive <br /> (Adapté pour tous supports)
               </li>
               <li>
                 Mise en place du contenu <br /> (Support fournis par vos soin)
               </li>
-              <li >Formulaire de contact & Map </li>
+              <li>Formulaire de contact & Map </li>
               <li>Mise en place des lien réseaux sociaux</li>
-              <li >Optimisation du référencement (SEO)</li>
-              <li >Mise en ligne du site</li>
-              <li >Formation utilisation du site  <br /> Personalisé</li>
+              <li>Optimisation du référencement (SEO)</li>
+              <li>Mise en ligne du site</li>
+              <li>
+                Formation utilisation du site <br /> Personalisé
+              </li>
             </ul>
           </div>
           <div>
-            
-          </div>
-          <div className="Services_mesure">
+           <div className="Services_mesure">
             <div className="Service">
               <h3>Service sur mesure</h3>
-              <h2>350€/h</h2>
+              <h2>300€/h</h2>
             </div>
             <p>
               Service personalisé/ <br /> Développement sur mesure <br /> (Tarif
               horaire)
             </p>
+          </div> 
+          <div className="pad"></div>
+          <div className="Services_mesure">
+            <div className="Service">
+              
+              <h2>400€/An**</h2>
+            </div>
+            <ul>
+              <li>Mise à jour site inernet &plugins</li>
+              <li>Petite modification</li>
+              <li>Restauration en cas de piratage</li>
+              <li>Sauvegardes mensuelles extérnalisées</li>
             
+  
+            </ul>
+           
+          </div> 
           </div>
           
           
           
         </div>{" "}
-        <h5 className="container_QuiSuisJe_title">*Conformément a l'aricle 296B du code général des impôts TVA non applicable </h5></div>
+        
+        <h5 className="container_QuiSuisJe_title">
+          *Conformément a l'aricle 296B du code général des impôts TVA non
+          applicable.{" "}
+        </h5>
+        <h5 className="container_QuiSuisJe_title">
+          **Calcul fait pour 2 heures de travails mensuelles sur votre sites.{" "}
+        </h5>
+      </div>
     </>
   );
 };
