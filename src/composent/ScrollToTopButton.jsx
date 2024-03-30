9ce394f1ce643addb480487235d6d3adef4031c9
Marsaud olivier <cougies79700@gmail.com>
2024-03-30 15:41:50 +0100

    add

--- src/composent/ScrollToTopButton.jsx
+++ src/composent/ScrollToTopButton.jsx
@@ -33,7 +33,19 @@ const ScrollToTopButton = () => {
     <div>
       {isVisible && (
         <button className="scroll-to-top-btn" onClick={scrollToTop}>
-          Remonter en haut
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          >
+            <polyline points="18 15 12 9 6 15"></polyline>
+          </svg>
         </button>
       )}
     </div>
