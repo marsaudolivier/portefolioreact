08619db856978b13cf9fd04299bde3f88d2bb304
Marsaud olivier <cougies79700@gmail.com>
2024-03-30 22:16:17 +0100

    try

--- src/composent/Head.jsx
+++ src/composent/Head.jsx
@@ -7,16 +7,13 @@ const Head = () => {
         <a href="./">
           <img src="assets/images/logo.svg" alt="logo" className="logo" />
         </a>
-
         <h1>Marsaud olivier développement</h1>
       </div>
       <div className="menu-bar">
         <ul className="nav " id="nav">
-
           <li>
             <a href="/projets">Voir mes projets</a>
           </li>
-      
         </ul>
       </div>
     </nav>
